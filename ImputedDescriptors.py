d528a4e53cf7f9e9a126a6e745e1ca5ceb330866
edg2103 <edg@icsi.berkeley.edu>
2019-05-06 11:46:45 -0400

    Update ImputedDescriptors.py

--- ImputedDescriptors.py
+++ ImputedDescriptors.py
@@ -13,6 +13,7 @@ import scipy.stats as stats
 import os
 import pickle
 import mol_utils as mu
+import sys
 
 # set the size of the training and test data (as a proportion of the perceptual ratings).  If test_size=1.0, we use only distributional data to create the model.
 test_size = 0.1
