402f967bcbdc8a9a3001236943199c70f22cbec2
edg2103 <edg@icsi.berkeley.edu>
2019-05-15 11:44:06 -0400

    Update DirectDescriptors.py

--- DirectDescriptors.py
+++ DirectDescriptors.py
@@ -18,7 +18,7 @@ import mol_utils as mu
 import sys
 
 # set the size of the training and test data (as a proportion of the perceptual ratings).  If test_size=1.0, we use only distributional data to create the model.
-test_size = 0.8
+test_size = 1.0
 
 modelType = 'FT0' #'FT0' or 'word2vec' or 'FT1' or 'FT2'
 
