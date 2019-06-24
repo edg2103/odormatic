de2c9d8f4352648446d9599db4789452a9f3af7c
edg2103 <edg@icsi.berkeley.edu>
2019-06-24 15:53:44 -0400

    Update ImputedMolecules.py

--- ImputedMolecules.py
+++ ImputedMolecules.py
@@ -18,7 +18,7 @@ import mol_utils as mu
 import sys
 
 # set the size of the training and test data (as a proportion of the perceptual ratings).  If test_size=1.0, we use only distributional data to create the model.
-test_size = 0.8
+test_size = 0.80
 
 # Set the distributional Semantic Model from which the word vectors to be used are derived -- e.g. FastText (FT0) or word2vec
 modelType = 'FT0'
